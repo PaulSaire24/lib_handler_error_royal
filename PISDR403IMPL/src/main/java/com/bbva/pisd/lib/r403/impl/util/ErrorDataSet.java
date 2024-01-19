394468a310b03062cce399fd83c2684cccc8ccdc
paulkyev <paul.saire.contractor@bbva.com>
2024-01-19 11:29:06 -0500

    add more errorin feature

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/ErrorDataSet.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/ErrorDataSet.java
@@ -3,7 +3,10 @@ package com.bbva.pisd.lib.r403.impl.util;
 public enum ErrorDataSet {
     ERROR_QUOTATION(ErrorHandlerType.EXTERNAL_RIMAC,"COT","servicio cotizacion",
             new ErrorMapper[]{ErrorMapper.COT0002001,
-                            ErrorMapper.COF0002003});
+                    ErrorMapper.COF0002003}),
+    ERROR_THIRD_VALIDATION(ErrorHandlerType.EXTERNAL_RIMAC,"THIRD","validación agregar terceros",
+            new ErrorMapper[]{ErrorMapper.COF000001,
+                    ErrorMapper.COF0002003});
 
     private final ErrorHandlerType typeError;
     private final String code;
