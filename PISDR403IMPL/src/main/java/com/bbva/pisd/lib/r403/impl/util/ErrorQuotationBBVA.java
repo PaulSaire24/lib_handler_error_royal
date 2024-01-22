a5f19faa9c8abf561dfdb4bc65508adf3b0e8e36
paulkyev <paul.saire.contractor@bbva.com>
2024-01-22 15:26:03 -0500

    PERAR-25146 PERAR-25740 refact: correction sonar Que duplicate

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/ErrorQuotationBBVA.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/ErrorQuotationBBVA.java
@@ -3,14 +3,14 @@ package com.bbva.pisd.lib.r403.impl.util;
 public enum ErrorQuotationBBVA {
     BBVA00123457("BBVA00123457","El campo producto es requerido","wrong"),
     BBVA00123464("BBVA00123464","El campo valor de datosParticulares en su elemento 1 debe contener como máximo 7 caracteres","Alert"),
-    BBVA00123465("BBVA00123465","No existe el valor de equivalencia para el tipo de documento enviado.","wrong"),
+    BBVA00123465("BBVA00123465","No existe el valor de equivalencia para el tipo de documento enviado.","internal server error"),
     BBVA00123466("BBVA00123466","El campo nroDocumento de persona en su elemento 1 debe contener 8 caracteres","Not Found"),
     BBVA00123467("BBVA00123467","El campo nroDocumento de persona en su elemento 1 con valor \\\"@\\\" no coincide para el patrón: 0-9","Forbidden"),
     BBVA00123468("BBVA00123468","El campo nroDocumento de persona en su elemento 1 debe contener como máximo 11 caracteres","Unauthorized"),
     BBVA00123469("BBVA00123469","El campo nroDocumento de persona en su elemento 1 debe contener al menos 5 caracteres","Bad Request"),
     BBVA00123470("BBVA00123470","El campo apePaterno de persona en su elemento 1 es requerido","Request Timeout"),
     BBVA00123471("BBVA00123471","El campo apePaterno de persona en su elemento 1 con valor \\\"@@\\\" no coincide para el patrón: a-zA-ZÀ-ÿ0-9_.&'\\\" -","Not Acceptable"),
-    BBVA00123472("BBVA00123472","El campo apePaterno de persona en su elemento 1 debe contener como máximo 30 caracteres","wrong"),
+    BBVA00123472("BBVA00123472","El campo apePaterno de persona en su elemento 1 debe contener como máximo 30 caracteres","TimeOut"),
     BBVA00123473("BBVA00123473","El campo apeMaterno de persona en su elemento 1 con valor \\\"@@\\\" no coincide para el patrón: a-zA-ZÀ-ÿ0-9_.&'\\\" -","Conflict"),
     BBVA00123474("BBVA00123474","El campo apeMaterno de persona en su elemento 1 es requerido","Bad Gateway");
 
