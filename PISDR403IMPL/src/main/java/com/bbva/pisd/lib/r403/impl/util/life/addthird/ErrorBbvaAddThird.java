1567881759b956f9696fc7f57eb0ad4781968651
paulkyev <paul.saire.contractor@bbva.com>
2024-02-14 17:33:15 -0500

    PERAR-26402 PERAR-26595 style: fix error orthographic

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/life/addthird/ErrorBbvaAddThird.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/life/addthird/ErrorBbvaAddThird.java
@@ -2,7 +2,7 @@ package com.bbva.pisd.lib.r403.impl.util.life.addthird;
 
 public enum ErrorBbvaAddThird {
 
-    BBVA00123465("BBVA00123465","El tipo de documento enviado no es valido","internal server error"),
+    BBVA00123465("BBVA00123465","El tipo de documento enviado no es válido","internal server error"),
     BBVA00123466("BBVA00123466","Número de Documento enviado debe contener 8 caracteres","Not Found"),
     BBVA00123467("BBVA00123467","Número de Documento enviado debe contener caracteres entre: 0-9","Forbidden"),
     BBVA00123468("BBVA00123468","Número de Documento enviado debe contener 11 caracteres","Unauthorized"),
@@ -14,9 +14,9 @@ public enum ErrorBbvaAddThird {
     BBVA00123474("BBVA00123474","El apellido Materno de la persona es requerido","Bad Gateway"),
     BBVA00123475("BBVA00123475","El apellido Materno debe contener como maximo 30 caracteres","Bad Gateway"),
     BBVA00123476("BBVA00123476","El nombre de la persona es requerido",""),
-    BBVA00123477("BBVA00123477","El numero de celula debe contener al menos 9 caracteres",""),
-    BBVA00123478("BBVA00123478","La fecha de nacimiento debe tener un formato valido",""),
-    BBVA00123479("BBVA00123479","El campo numero de via es requerido","");
+    BBVA00123477("BBVA00123477","El número de celular debe contener al menos 9 caracteres",""),
+    BBVA00123478("BBVA00123478","La fecha de nacimiento debe tener un formato válido",""),
+    BBVA00123479("BBVA00123479","El campo número de vía es requerido","");
     private final String code;
     private final String message;
     private final String typeAlert;
