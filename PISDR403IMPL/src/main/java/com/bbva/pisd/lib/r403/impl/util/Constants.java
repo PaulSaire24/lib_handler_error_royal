e16bd0c85a81ddbc0120392f9c22555ede19413e
paulkyev <paul.saire.contractor@bbva.com>
2024-03-25 12:32:16 -0500

    PERAR-27041 fix: code smell

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/Constants.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/Constants.java
@@ -3,17 +3,26 @@ package com.bbva.pisd.lib.r403.impl.util;
 public class Constants {
 
     public final class ErrorType{
+        private ErrorType() {
+        }
+
         public static final String ERROR_RIMAC = "RIMAC";
         public static final String ERROR_HOST = "HOST";
         public static final String ERROR_APX = "APX";
     }
 
     public final class Error{
+
+        private Error() {
+        }
         public static final String CODE = "CODE";
         public static final String DETAIL = "DETAIL";
     }
 
     public final class Columns{
+        private Columns() {
+        }
+
         public static final String CATALOG_ELEMENT_DESC = "CATALOG_ELEMENT_DESC";
         public static final String PRODUCT_TYPE = "PRODUCT_TYPE";
         public static final String CATALOG_ELEMENT_ID = "CATALOG_ELEMENT_ID";
