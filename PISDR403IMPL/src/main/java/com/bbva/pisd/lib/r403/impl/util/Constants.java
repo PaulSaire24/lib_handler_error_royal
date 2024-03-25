896d986cf4237765f5045f719f9f85ab9bb570a2
paulkyev <paul.saire.contractor@bbva.com>
2024-03-25 12:58:22 -0500

    PERAR-27041 fix: code smell in constructor private

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/Constants.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/util/Constants.java
@@ -2,9 +2,10 @@ package com.bbva.pisd.lib.r403.impl.util;
 
 public class Constants {
 
+    private Constants() {
+    }
+
     public final class ErrorType{
-        private ErrorType() {
-        }
 
         public static final String ERROR_RIMAC = "RIMAC";
         public static final String ERROR_HOST = "HOST";
@@ -13,15 +14,11 @@ public class Constants {
 
     public final class Error{
 
-        private Error() {
-        }
         public static final String CODE = "CODE";
         public static final String DETAIL = "DETAIL";
     }
 
     public final class Columns{
-        private Columns() {
-        }
 
         public static final String CATALOG_ELEMENT_DESC = "CATALOG_ELEMENT_DESC";
         public static final String PRODUCT_TYPE = "PRODUCT_TYPE";
