f932be2f3a494c070c1833e75392b6cfe848751b
paulkyev <paul.saire.contractor@bbva.com>
2024-04-03 17:31:36 -0500

    PARSRBVD-1955 PARSRBVD-2045: feat: query modify by channel and code error

--- PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/map/ErrorMap.java
+++ PISDR403IMPL/src/main/java/com/bbva/pisd/lib/r403/impl/map/ErrorMap.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import com.bbva.pisd.lib.r403.impl.util.Constants;
 
+
 public class ErrorMap {
 
     public static Map<String,Object> getArgumentsForQuery(List<String> arrayCodes, String codeError,String channel){
@@ -16,8 +17,10 @@ public class ErrorMap {
         if(argumentsCodes.length()>0){
             argumentsCodes.deleteCharAt(argumentsCodes.length()-1);
         }
+
+        String shortCode = codeError.substring(0,4);
         arguments.put(Constants.CATALOG_ELEMENT_ID,argumentsCodes.toString());
-        arguments.put(Constants.PRODUCT_TYPE,reference);
+        arguments.put(Constants.CHANNEL,channel.concat(Constants.getCodeByName(shortCode)));
         return arguments;
     }
 }
